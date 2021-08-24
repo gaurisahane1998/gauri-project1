dc90d3156885e9fc9a27a37279ccc7367d001a2f
gaurisahane1998 <sahanegauri706@gmail.com>
2021-08-24 15:08:34 +0530

    project1 completed

--- consolebasedApp-onlineshopping-e-mall-master/consolebasedApp-onlineshopping-e-mall-master/src/test/java/com/mall/testcases/Testcases.java
+++ consolebasedApp-onlineshopping-e-mall-master/consolebasedApp-onlineshopping-e-mall-master/src/test/java/com/mall/testcases/Testcases.java
@@ -12,35 +12,15 @@ import com.mall.customerRegister.service.CustomerRegisterService;
 import com.mall.Dao.CustomerLoginDAO;
 import com.mall.Dao.ProductAddDAO;
 import com.mall.Dao.Impl.CustomerLoginDAOImpl;
+import com.mall.Dao.Impl.CustomerSearchDAOImpl;
 import com.mall.Dao.Impl.MarkAsShippedDAOImpl;
 import com.mall.Dao.Impl.ProductAddDAOImpl;
 import com.mall.Exception.BusinessException;
 import com.mall.model.Cart;
 import com.mall.model.Customer;
 import com.mall.model.Employee;
-import com.mall.model.Order;
-import com.mall.model.OrderId;
 import com.mall.model.Product;
-import com.mall.search.service.CustomerSearchService;
-import com.mall.search.service.Impl.CustomerSearchServiceImpl;
-import com.mall.service.AddProductToCartService;
-import com.mall.service.CustomerLoginService;
-import com.mall.service.ListProductsService;
-import com.mall.service.MarkOrderStatusService;
-import com.mall.service.PlaceAnOrderService;
-import com.mall.service.ViewAllOrdersService;
-import com.mall.service.ViewCartService;
-import com.mall.service.ViewMyOrdersService;
-import com.mall.service.impl.AddProductToCartServiceImpl;
-import com.mall.service.impl.CustomerLoginServiceImpl;
-import com.mall.service.impl.ListProductsServiceImpl;
-import com.mall.service.MarkAsShippedService;
-import com.mall.service.impl.MarkAsShippedServiceImpl;
-import com.mall.service.impl.MarkOrderStatusServiceImpl;
-import com.mall.service.impl.PlaceAnOrderServiceImpl;
-import com.mall.service.impl.ViewAllOrderServiceImpl;
-import com.mall.service.impl.ViewCartServiceImpl;
-import com.mall.service.impl.ViewMyOrdersServiceImpl;
+
 
 public class Testcases {
 private static Logger log = Logger.getLogger(Testcases.class);
@@ -49,47 +29,117 @@ private static Logger log = Logger.getLogger(Testcases.class);
 
 	@Test
 	public void validemployee() { 			//checking for valid details
-		assertEquals(1,employee.employeValidate("praneethsalla@gmail.com", "mintu123"));
+		assertEquals(1,employee.employeValidate("gauri@gmail.com", "123"));
 	}
 	/*
 	@Test
 	public void invalidemployeedetails() {				//not valid
-		assertEquals(1,employee.employeValidate("pranithsalla@gmail.com", "Mintu@123"));
+		assertEquals(1,employee.employeValidate("gauri@gmail.com", "123"));
+	
 		
 	}*/
 
 CustomerLoginDAOImpl obj = new CustomerLoginDAOImpl();
+
+private CustomerRegisterServiceImpl customerRegister;
 	
 	@Test
 	public void testCustomerLogin() {
 		try {
 			boolean result = true;
-			assertEquals(result,obj.customerLogin("madhu@gmail.com", "as@AS1cgrr"));
+			assertEquals(result,obj.customerLogin("pari@gmail.com", "Pari@123"));
 		}catch (BusinessException e) {}
-			/*fail(e.getMessage());
-		}
 	}
 
-	/*
-MarkAsShippedDAOImpl markShipped = new MarkAsShippedDAOImpl();
-	
-	@Test
-	public void testMarkShipped() {
-		Order order = new Order();
-		order.setOrderId(10);
-		try {
-			assertEquals(1,markShipped.markDelivered(order));
-		} catch (BusinessException e) {
+/*
 			fail(e.getMessage());
 		}
 	}
-	
+}*/
 
 
-	*/
-		
-	
-	
+ProductAddServiceImpl productAdd = new ProductAddServiceImpl();
+
+@Test
+public void testProductAdd() {
+	Product product = new Product();
+	product.setId(16);
+	product.setName("vivoY22");
+	product.setManufacturer("vivo");
+	product.setCost(450000);
+	try {
+		assertEquals(1,productAdd.addProduct(product));
+	} catch (BusinessException e) {}
+}
+		/*fail(e.getMessage());
+	}
+*/
+CustomerSearchDAOImpl customerSearch = new CustomerSearchDAOImpl();
+Customer customer = new Customer();
+
+@Test
+public void testCustomerSearchById() {
+	try {
+		int id = 1;
+		assertNotNull(customerSearch.getCustomerById(id));
+	} catch (BusinessException e) {}
+}
+	/*	fail(e.getMessage());
 	}
 }
+*/
+@Test
+public void testCustomerSearchByName() {
+	try {
+		assertNotNull(customerSearch.getCustomerByName("pari"));
+	} catch (BusinessException e) {}
+}
+	/*
+		fail(e.getMessage());
+	}
+}
+*/
+@Test
+public void testCustomerSearchByEmail() {
+	try {
+		assertNotNull(customerSearch.getCustomerByEmail("pari@gmail.com"));
+	} catch (BusinessException e) {}
+}
+		/*fail(e.getMessage());
+	}
+}
+*/
+@Test
+public void testCustomerSearchByOrderId() {
+	try {
+		int id = 1;
+		assertNotNull(customerSearch.getCustomerByOrderId(id));
+	} catch (BusinessException e) {}
+}
+	/*
+		fail(e.getMessage());
+	}
+}
+*/
 
+
+CustomerRegisterServiceImpl CustomerRegister = new CustomerRegisterServiceImpl();
+
+@Test
+public void testCustomerRegister() {
+	Customer customer = new Customer();
+	customer.setCustomerId(15);
+	customer.setCustomerEmail("mona@gmail.com");
+	customer.setCustomerName("mona");
+	customer.setCustomerPassword("Mona@123");
+	try {
+		assertEquals(1,CustomerRegister.registerCustomer(customer));
+	} catch (BusinessException e) {}
+}
+}
+
+	/*	fail(e.getMessage());
+	}
+}
+*/
+  
